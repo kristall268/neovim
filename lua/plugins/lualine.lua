c574bbd74202e86930435a7800e3b0e41d1947cb
kristall268 <kristall268@outlook.com>
2025-07-24 22:31:07 +0500

    Applied finalize lualine and added plugin gitsigns

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,32 +3,48 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin/nvim" },
   config = function()
     require("lualine").setup({
-      options = {
-        theme = "catppuccin",
-        component_separators = { left = "│", right = "│" },
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = { 
-          statusline = { "dashboard", "alpha", "starter"},
-          winbar = {},
-        },
-        globalstatus = true, -- Enable global statusline
-      },
-      sections = {
-        lualine_a = { "mode", separator = { left = ""}, right_padding = 2 },
-        lualine_b = { "branch", {"diff", colored = true, symbols = { added = " ", modified = "", removed = " " }}, {"diagnostics",symbols = { error = " ", warn = " ", info = " ", hint = " " } } },
-        lualine_c = { "filename" },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      extensions = {},
-    })
+            options = {
+                theme = "catppuccin",
+                component_separators = { left = "│", right = "│" },
+                section_separators = { left = "", right = "" },
+                disabled_filetypes = { 
+                    statusline = { "dashboard", "alpha", "starter"},
+                    winbar = {},
+                },
+                globalstatus = true, -- Enable global statusline
+            },
+            sections = {
+                lualine_a = {
+                    {"mode", separator = { left = ""}, right_padding = 2}, 
+                },
+                lualine_b = {
+                    "branch", 
+                    {
+                        "diff", colored = true, symbols = { added = " ", modified = "", removed = " " }
+                    }, 
+                    {
+                        "diagnostics",symbols = { error = "", warn = " ", info = "", hint = "" } 
+                    },
+                },
+                lualine_c = { 
+                    {"filename", path = 1}
+                },
+                lualine_x = { "encoding", "fileformat", "filetype" },
+                lualine_y = { "progress" },
+                lualine_z = { "location" },
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { "filename" },
+                lualine_x = { "location" },
+                lualine_y = {},
+                lualine_z = {},
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = { "nvim-tree", "lazy", "mason", "trouble"},
+        })
+    end,
 }
