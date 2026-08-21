df8a456acbb9f3850aa9965b4f05132a3daa663c
kristall268 <kristall268@outlook.com>
2026-08-21 06:13:55 +0000

    treesitter changed to branch "main"

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
--- ~/.config/nvim/lua/config/plugins/treesitter.lua
+-- ~/.config/nvim/lua/plugins/treesitter.lua
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -6,11 +6,40 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
+
+      local ensure_installed = { "nix", "lua", "bash", "markdown" }
+
       require("nvim-treesitter").setup({
-        ensure_installed = {"nix"},
-        auto_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
+        install_dir = vim.fn.stdpath("data") .. "/site",
+      })
+
+      -- Установка недостающих парсеров
+      local installed = require("nvim-treesitter.config").get_installed()
+      local to_install = vim.iter(ensure_installed)
+        :filter(function(lang)
+          return not vim.tbl_contains(installed, lang)
+        end)
+        :totable()
+
+      if #to_install > 0 then
+        require("nvim-treesitter").install(to_install)
+      end
+
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(ev)
+          local lang = vim.treesitter.language.get_lang(ev.match)
+          if not lang then
+            return
+          end
+          local ok = pcall(vim.treesitter.language.add, lang)
+          if not ok then
+            return
+          end
+          vim.treesitter.start(ev.buf, lang)
+
+          vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
       })
     end,
   },
